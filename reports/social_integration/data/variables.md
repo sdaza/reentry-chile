b845b4b05e9112e6a2f238af2be03b14d9fc7530
sdaza <sebastian.daza@gmail.com>
2019-04-21 08:01:28 -0700

    create social integration report

---
+++ reports/social_integration/data/variables.md
@@ -0,0 +1,47 @@
+
+# variable definition
+
+# dependent variables
+
+##  support from family
+
+- received money from family
+- live with a family member
+- family relationship
+
+Western definition: We constructed a measure of family support that
+indicated respondents who received money from family in the month of the survey
+or who were staying with family.
+
+## marginal temporary housing
+
+- To study the housing of released prisoners, we distinguish marginal and
+ temporary living situations deﬁned as residing in a homeless shelter,
+ a sober house or residential program, a rooming house ðusually paid weekly,
+ or a hotel or motel; staying at multiple residences;
+ or being homeless on the streets or in a correctional facility
+
+## currently employed
+
+- legally employed / informal job
+
+## public assistance
+
+- received money from programs
+- enroll in social programs
+
+# independent variables
+
+I would not include measures of the first week if we are predicting
+what happend after release.
+
+- mental health previous period or only at based line
+- quality relation with family (index)
+- isolation index:
+    - A continuous standardized index of time spent without family and in no activity in the ﬁrst week after release
+- age
+- mother
+- drug dependency at baseline
+- type of sentence
+- crime involment
+- early crime risk exposure
---
+++ reports/social_integration/src/01_load_data.R
@@ -0,0 +1,63 @@
+###########################
+# social integration report
+# load data
+# author: sebastian daza
+############################
+
+library(sdazar)
+
+# first week
+
+dps = fread('data/180829_1_primerasemana.csv')
+setnames(dps, names(dps), tolower(names(dps)))
+
+# temporary housing
+spent_night = lookvar(dps, 'p20_c_dia[0-9]$')
+dps = assmis(dps, list(spent_night), list(c(-99, -88)))
+table(dps[, spent_night[1], with=FALSE], useNA='ifany')
+
+dps[, temp_housing := apply(.SD, 1,
+                            function(x) as.numeric(any(x %in% c(1, 11, 12, 13, 7, 8)))),
+    .SDcols=spent_night]
+
+table(dps$temp_housing)
+
+# money from familiy (including partner)
+dps = assmis(dps, list(c('p43_4_a', 'p43_3_a')), list(c(-9, -8, -99, -88)))
+table(dps$p43_4_a, useNA='ifany')
+
+dps[, money_family := apply(.SD, 1,
+                            function(x) as.numeric(any(x == 1))),
+    .SDcols=c('p43_4_a', 'p43_3_a')]
+table(dps$money_family, useNA='ifany')
+
+# living with family
+dps[, living_with_family := apply(.SD, 1,
+                            function(x) as.numeric(any(x %in% 4:6))),
+    .SDcols=spent_night]
+
+table(dps$living_with_family, useNA='ifany')
+
+dps[, family_support := apply(.SD, 1,
+                            function(x) as.numeric(any(x == 1))),
+    .SDcols=c(living_with_family, money_family)]
+
+table(dps$family_support, useNA='ifany']
+
+# any job
+dps = assmis(dps, list('p22'), list(c(-9, -8, -99, -88)))
+dps[, work := p22]
+
+table(dps$work, useNA='ifany')
+
+# public services
+money_public_programs = c('p43_1_a', 'p43_2_a')
+dps = assmis(dps, list(money_public_programs), list(c(-9, -8, -99, -88)))
+
+dps[, money_pp := apply(.SD, 1,
+                        function(x) as.numeric(any(x == 1))),
+    .SDcols=money_public_programs]
+
+table(dps$money_pp, useNA='ifany')
+
+
