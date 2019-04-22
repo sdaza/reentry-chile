cbcae53950c46d58c6aa731246d6bf566b2337ea
sdaza <sebastian.daza@gmail.com>
2019-04-21 20:27:07 -0700

    Update 00_source.R

---
+++ reports/social_integration/src/00_source.R
@@ -0,0 +1,8 @@
+###########################
+# social integration report
+# source file for reproducibility
+# author: sebastian daza
+############################
+
+
+source('social_integration/src/01_load_data.R')
